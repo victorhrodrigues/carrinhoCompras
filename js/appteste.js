0f1dd7f25c2e6e24e823a8a3a60a9230baac1f7a
victorhrodrigues <victorhrodrigues64@gmail.com>
2024-11-26 20:35:31 -0300

    finalizando o projeto

--- js/appteste.js
+++ js/appteste.js
@@ -1,5 +1,7 @@
 let valorTotal = 0;
 
+limpar();
+
 function adicionar(){
     // recuperando os valores do produt0, quantidade e valor
     let produto = document.getElementById('produto').value; //devolve toda a string da tag
@@ -7,6 +9,17 @@ function adicionar(){
     let valorUnitario = produto.split('R$')[1]; // posição [0] -> antes da quebra. posição [1] -> depois da quebra
     let quantidadeProduto = document.getElementById('quantidade').value;
     
+    // verificação de produto é válido
+    if(produto.trim() === '' || !produto){
+        alert('Selecione um produto válido');
+    }
+   
+    // verifica se a quantidade é valida
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
     //calcular o preço e o subtotal
     let preco = (quantidadeProduto) * valorUnitario;
 
@@ -16,11 +29,17 @@ function adicionar(){
     <span class="texto-azul">${quantidadeProduto}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
     </section>`;
     //atualizar valor total
-    valorTotal = document.getElementById('valor-total');
-    console.log(valorTotal);
+    valorTotal = valorTotal + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${valorTotal}`;
+    document.getElementById('quantidade').value = "";
 }
 
 function limpar(){
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML = '';
+    valorTotal = 0;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${valorTotal}`
+    document.getElementById('quantidade').value = "";
 }
